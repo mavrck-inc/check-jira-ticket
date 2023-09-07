3964b9e5151921e5ff786965ef04566eec8da0ff
Razvan Bucurica <bucurica.razvan@gmail.com>
2023-09-07 15:40:23 +0300

    debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,11 +18,26 @@ fi
 
 shopt -u nocasematch
 
+
+urls=("${JIRA_SERVER}" "https://${JIRA_SERVER}")
+
+for url in "${urls[@]}"
+do
+  status_code=$(curl -I -s -o /dev/null -w "%{http_code}" "$url")
+  if [ "$status_code" -eq 200 ]; then
+    echo "$url: OK"
+  else
+    echo "$url: Failed with status $status_code"
+  fi
+done
+
 response=$(curl --request GET \
               --url "https://${JIRA_SERVER}/rest/api/3/issue/${JIRA_TICKET}" \
               --user "${JIRA_SERVICE_USER}:${JIRA_SERVICE_API_TOKEN}" \
               --header "Accept: application/json" \
               --silent)
+
+echo "response: $response"
 
 if [[ $(echo "$response" | jq -r '.key') == "$JIRA_TICKET" ]]; then
 echo "Valid JIRA ticket!"
