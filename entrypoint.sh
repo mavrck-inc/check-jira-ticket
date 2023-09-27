09264fd91cae3d1fdff66264ba9687cc695023ed
Razvan Bucurica <bucurica.razvan@gmail.com>
2023-09-27 14:19:52 +0300

    added githubToken

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ REPO=${REPO}
 # Use the GitHub API to get the latest title and body of the PR
 PR_DATA=$(curl --request GET \
                --url "https://api.github.com/repos/$REPO/pulls/$PR_NUMBER" \
-               --header "Authorization: Bearer ${{ secrets.GH_API_TOKEN }}" \
+               --header "Authorization: Bearer $GH_API_TOKEN" \
                --header "Accept: application/vnd.github.v3+json" \
                --silent)
 
